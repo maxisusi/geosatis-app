f432a8038a325d3d6a399ea8bde78f93f6c85527
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-07 09:49:55 +0200

    Adding map on component

--- src/app/components/offenders-map/offenders-map.component.ts
+++ src/app/components/offenders-map/offenders-map.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { circle, latLng, polygon, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-offenders-map',
   templateUrl: './offenders-map.component.html',
-  styleUrls: ['./offenders-map.component.css']
+  styleUrls: ['./offenders-map.component.css'],
 })
 export class OffendersMapComponent implements OnInit {
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...',
+      }),
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909),
+  };
 
-  constructor() { }
+  layersControl = {
+    baseLayers: {
+      'Open Street Map': tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        { maxZoom: 18, attribution: '...' }
+      ),
+      'Open Cycle Map': tileLayer(
+        'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+        { maxZoom: 18, attribution: '...' }
+      ),
+    },
+    overlays: {
+      'Big Circle': circle([46.95, -122], { radius: 5000 }),
+      'Big Square': polygon([
+        [46.8, -121.55],
+        [46.9, -121.55],
+        [46.9, -121.7],
+        [46.8, -121.7],
+      ]),
+    },
+  };
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
