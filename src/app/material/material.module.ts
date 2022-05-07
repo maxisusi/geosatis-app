d47d86e63ab6b04bb36e846f475ed158de1fceb9
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-07 10:36:21 +0200

    Adding MUI pagination component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { MatPaginatorModule } from '@angular/material/paginator';
 const MaterialComponents = [
   MatButtonModule,
   MatToolbarModule,
   MatIconModule,
   MatSlideToggleModule,
+  MatPaginatorModule,
 ];
 
 @NgModule({
