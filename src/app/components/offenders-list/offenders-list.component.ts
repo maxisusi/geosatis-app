74bee8b728272b47fe29fe2f9806ffe8d1173ce4
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-08 10:09:09 +0200

    Binding datas to offender-list component

--- src/app/components/offenders-list/offenders-list.component.ts
+++ src/app/components/offenders-list/offenders-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Offender } from 'src/app/shared/application.models';
 
 @Component({
   selector: 'app-offenders-list',
   templateUrl: './offenders-list.component.html',
-  styleUrls: ['./offenders-list.component.css']
+  styleUrls: ['./offenders-list.component.css'],
 })
 export class OffendersListComponent implements OnInit {
+  @Input() offender!: Offender;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
