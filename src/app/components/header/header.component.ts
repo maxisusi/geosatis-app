6f33869a4af374378da251cee3c1eaa32b2e49c0
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-09 08:40:03 +0200

    Timer is updating in realtime

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment, MomentTimezone } from 'moment-timezone';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import * as moment from 'moment';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  date = new Date().toString();
+  ticker$ = interval(1000);
 
   ngOnInit(): void {
+    this.ticker$.subscribe(() => {
+      this.date = moment().format('MMMM Do YYYY, h:mm:ss a');
+    });
   }
-
 }
