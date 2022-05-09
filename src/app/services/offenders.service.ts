bf12aec36d6aa10d3da2a4658917462ef735dcdf
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-09 19:47:37 +0200

    Fixing Overpopulation of created offenders

--- src/app/services/offenders.service.ts
+++ src/app/services/offenders.service.ts
@@ -64,23 +64,6 @@ export class OffendersService {
   }
 
   createOffender(offender: Offender): Observable<Offender> {
-    // const { birthdate, firstName, imgURL, lastName }: Offender = offender;
-
-    console.log(offender);
-
-    // * Object Redefinition
-    // const formValues: Offender = {
-    //   id: uuidv4(),
-    //   firstName,
-    //   lastName,
-    //   birthdate,
-    //   location: {
-    //     lat: 46.132335832224506,
-    //     long: 7.075798217929714,
-    //   },
-    //   imgURL: 'https://source.unsplash.com/800x800/?face',
-    // };
-
     return this.http.post<Offender>(this.apiUrl, offender, this.httpOptions);
   }
 
--- src/app/store/state/offenders/offenders.selectors.ts
+++ src/app/store/state/offenders/offenders.selectors.ts
@@ -8,7 +8,7 @@ export const selectOffenders = (state: AppState) => state.offenders;
 // * Select all offenders
 export const selectAllOffenders = createSelector(
   selectOffenders,
-  (state: OffenderState) => state.offenders
+  (state: OffenderState) => state.offenders.slice(0, 5)
 );
 
 // * Select offender by ID
@@ -16,21 +16,3 @@ export const getOffenderById = (id: string) =>
   createSelector(selectOffenders, (state: OffenderState) => {
     return state.offenders.find((offender: Offender) => offender.id === id);
   });
-
-// export const getFirstOffender = (
-//   current: number,
-//   { min = 1, total = 20, length = 5 } = {}
-// ) =>
-//   createSelector(selectOffenders, (state: OffenderState) => {
-//     function getPagingRange() {
-//       if (length > total) length = total;
-
-//       let start = current - Math.floor(length / 2);
-//       start = Math.max(start, min);
-//       start = Math.min(start, min + total - length);
-
-//       return Array.from({ length: length }, (el, i) => start + i);
-//     }
-
-//     getPagingRange(current, tot);
-//   });
