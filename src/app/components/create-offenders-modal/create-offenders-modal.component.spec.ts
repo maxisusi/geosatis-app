60537c7b30e2ae4d2b4f28292f489ef1e50e5b7e
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-05-12 11:41:35 +0200

    All basic test works

--- src/app/components/create-offenders-modal/create-offenders-modal.component.spec.ts
+++ src/app/components/create-offenders-modal/create-offenders-modal.component.spec.ts
@@ -1,33 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { OffendersService } from 'src/app/services/offenders.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { CreateOffendersModalComponent } from './create-offenders-modal.component';
-import {
-  MatDialog,
-  MatDialogModule,
-  MatDialogRef,
-} from '@angular/material/dialog';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Offender } from 'src/app/shared/application.models';
 
 describe('CreateOffendersModalComponent', () => {
   let component: CreateOffendersModalComponent;
   let fixture: ComponentFixture<CreateOffendersModalComponent>;
 
-  let matData: any;
-  let dialog: MatDialog;
-  let store: MockStore;
+  let httpClient: HttpClient;
+  let httpHandler: HttpHandler;
+  // let store: MockStore;
+
+  let offenderListMock: Offender = {
+    id: '1edf1124-8de3-44d3-812a-93340080ace1',
+    firstName: 'Max',
+    lastName: 'balej',
+    birthdate: '01/10/22',
+    imgURL: 'https://source.unsplash.com/551x536/?face',
+    location: {
+      id: 6,
+      value: 'Base - Vevey',
+      coordinates: {
+        lat: 46.46194244121148,
+        long: 6.840651156969183,
+      },
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MatDialogModule, MAT_DIALOG_DATA],
-      providers: [OffendersService, provideMockStore({}), MatDialog],
+      imports: [MatDialogModule],
+      providers: [
+        OffendersService,
+        HttpClient,
+        HttpHandler,
+        provideMockStore({}),
+        { provide: MAT_DIALOG_DATA, useValue: { ...offenderListMock } },
+      ],
       declarations: [CreateOffendersModalComponent],
     }).compileComponents();
 
-    matData = TestBed.inject(MAT_DIALOG_DATA);
-    dialog = TestBed.inject(MatDialog);
+    // matData = TestBed.inject(MAT_DIALOG_DATA);
+
     // store = TestBed.inject(MockStore);
+
+    httpClient = TestBed.inject(HttpClient);
+    httpHandler = TestBed.inject(HttpHandler);
   });
 
   beforeEach(() => {
